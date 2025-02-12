63a0d647388e735ceca117693456ad3d907dc8f2
AngelMerchant <98000923+AngelMerchant@users.noreply.github.com>
2025-02-11 19:57:59 -0800

    Revert "Feature/legato 10"

--- checklogin.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# Get username and password from sendlogin in userLoginActivity
-username = sys.stdin.readline().strip()
-password = sys.stdin.readline().strip()
-
-if username == 'admin' & password == 'password':
-    print('correct login') #test code
